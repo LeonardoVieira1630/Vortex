58447ed07debd154a6e958994298667695917746
kiqsmg <kiqsmg@gmail.com>
2023-05-25 15:00:53 -0300

    Add new graph test index file in templates folder

--- FrontEnd/teste/script2.js
+++ FrontEnd/teste/script2.js
@@ -1,27 +1,15 @@
-function slideCards(container) {
-    const cards = container.querySelectorAll('.card');
-    let currentPosition = 0;
-    const cardWidth = cards[0].offsetWidth;
-    const numCards = cards.length;
-    
-    container.style.width = `${cardWidth*numCards}px`;
-    
-    function moveCards(position) {
-      container.style.transform = `translateX(-${position*cardWidth}px)`;
-    }
-    
-    // Next button click
-    document.querySelector('.next').addEventListener('click', () => {
-      currentPosition = Math.min(currentPosition + 1, numCards-1);
-      moveCards(currentPosition);
-    });
-    
-    // Prev button click
-    document.querySelector('.prev').addEventListener('click', () => {
-      currentPosition = Math.max(currentPosition - 1, 0);
-      moveCards(currentPosition);
-    });
-  }
-
-const cardSlider = document.querySelector('.card-slider');
-slideCards(cardSlider);
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 60,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
--- app.py
+++ app.py
@@ -3,25 +3,27 @@
 Aqui passaremos as informações de cada endpoint.
 """
 
-#Importações
+# Importações
 from flask import Flask, render_template, request, flash, redirect, jsonify
-import config, csv, datetime
+import config
+import csv
+import datetime
 from binance.client import Client
 from binance.enums import *
 from flask_cors import CORS
 
 
-#Criando nosso app com o flask
+# Criando nosso app com o flask
 app = Flask(__name__)
 CORS(app)
 app.secret_key = b'somelongrandomstring'
 
 
-#Para usar a conta real teremos que tirar a parte do teste net e mudar as keys
-client = Client(config.API_KEY,config.SECRET_KEY, testnet=True)
+# Para usar a conta real teremos que tirar a parte do teste net e mudar as keys
+client = Client(config.API_KEY, config.SECRET_KEY, testnet=True)
 
 
-#Principal end-point. 
+# Principal end-point.
 @app.route('/')
 def index():
     title = 'D.T.A.B'
@@ -35,47 +37,49 @@ def index():
 
     return render_template('index2.html', title=title, my_balances=balances, symbols=symbols)
 
+# Quando ordenarmos uma compra:
 
-#Quando ordenarmos uma compra:
+
 @app.route('/buy', methods=['POST'])
 def buy():
     print(request.form)
     try:
-        order = client.create_order(symbol=request.form['symbol'], 
-            side=SIDE_BUY,
-            type=ORDER_TYPE_MARKET,
-            quantity=request.form['quantity'])
+        order = client.create_order(symbol=request.form['symbol'],
+                                    side=SIDE_BUY,
+                                    type=ORDER_TYPE_MARKET,
+                                    quantity=request.form['quantity'])
     except Exception as e:
         flash(e.message, "error")
 
     return redirect('/')
 
 
-#Quando ordenarmos uma compra:
+# Quando ordenarmos uma compra:
 @app.route('/sell')
 def sell():
     return 'sell'
 
 
-#Se houver mudança nas settings do rsi:
+# Se houver mudança nas settings do rsi:
 @app.route('/settings')
 def settings():
     return 'settings'
 
 
-#End-point para as informações do grafico.
+# End-point para as informações do grafico.
 @app.route('/history')
 def history():
-    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_5MINUTE)
+    candlesticks = client.get_historical_klines(
+        "BTCUSDT", Client.KLINE_INTERVAL_5MINUTE)
 
     processed_candlesticks = []
 
     for data in candlesticks:
-        candlestick = { 
-            "time": data[0] / 1000, 
+        candlestick = {
+            "time": data[0] / 1000,
             "open": data[1],
-            "high": data[2], 
-            "low": data[3], 
+            "high": data[2],
+            "low": data[3],
             "close": data[4]
         }
         processed_candlesticks.append(candlestick)
